10c81b08f2f8184d64ffe8e632f24287cf5ab4af
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2021-09-10 00:12:22 -0300

    note sidebar

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,27 +1,36 @@
+import moment from 'moment';
 import React from 'react';
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, date, title, body, url }) => {
+
+    const noteDate = moment(date);
+
     return (
         <div className='journal__entry pointer'>
-            <div className='journal__entry-picture'
-                style = {{
-                    backgroundSize:'cover',
-                    backgroundImage:'url(https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg)'
-                }}
-            ></div>
+
+            {   
+                url &&
+                <div className='journal__entry-picture'
+                    style = {{
+                        backgroundSize:'cover',
+                        backgroundImage: `url(${url})`
+                    }}
+                ></div>
+            }
+            
 
             <div className='journal__entry-body'>
                 <p className='journal__entry-title'>
-                    Un buen día
+                    { title }
                 </p>
                 <p className='journal__entry-content'>
-                    Non culpa non quis occaecat proident veniam dolor velit labore.
+                    { body }
                 </p>
             </div>
 
             <div className='journal__entry-date-box'>
-                <span>Monday</span>
-                <h4>28</h4>
+                <span>{ noteDate.format('dddd')} </span>
+                <h4>{ noteDate.format('Do')}</h4>
             </div>
         </div>
     )
