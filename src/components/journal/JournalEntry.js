c9599e65a5df8b94c7bfdf8d2743373d25cdf398
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2021-09-11 17:07:25 -0300

    show note active

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,12 +1,24 @@
 import moment from 'moment';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 
 export const JournalEntry = ({ id, date, title, body, url }) => {
 
     const noteDate = moment(date);
 
+    const dispatch = useDispatch();
+
+    const handleEntryClick = () => {
+        dispatch( activeNote( id, {
+            date, title, body, url
+        }));
+    }
+
     return (
-        <div className='journal__entry pointer'>
+        <div className='journal__entry pointer'
+            onClick={ handleEntryClick }
+        >
 
             {   
                 url &&
